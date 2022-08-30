10164d6649d2ada2da8b2aba2c7f226ca08ae487
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-08-30 17:56:10 +0300

    FIX: colab version

--- colab_edition/__init__.py
+++ colab_edition/__init__.py
@@ -6,9 +6,14 @@ def render_photo(path):
     try:
         img = open(path, 'rb').read()
     except:
-        img = open('img/artifacts/key.png').read()
+        img = open('diohod_bot/img/artifacts/dino.png', 'rb').read()
     data_url = 'data:image/jpeg;base64,' + b64encode(img).decode()
     return data_url
 
+
 def go():
-    display(HTML(f"<img src='{render_photo('img/artifacts/key.png')}' width='130'>"))
+    display(HTML(f"<img src='{render_photo('diohod_bot/img/artifacts/key.png')}' width='130'>"))
+
+
+# img = open('img/artifacts/dino.png', 'rb').read()
+# go()
